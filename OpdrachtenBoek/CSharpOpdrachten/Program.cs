a4104a924e5941001f06eea434940f58f48eb342
ALi <aaali2@st.noorderpoort.nl>
2023-12-20 12:00:57 +0100

    Gewerkt tot 22

--- OpdrachtenBoek/CSharpOpdrachten/Program.cs
+++ OpdrachtenBoek/CSharpOpdrachten/Program.cs
@@ -5,10 +5,15 @@ class Program
     static void Main(string[] args)
     {
         // comment de hoofdstuk die u wilt controleren
-       /* Schrijven schrijven = new Schrijven();
-        schrijven.Schrijft();*/
-        Lezen lezen = new Lezen();
-        lezen.Leest();
+       //Schrijven schrijven = new Schrijven();
+       //schrijven.Schrijft();
+       
+       //Lezen lezen = new Lezen();
+       //lezen.Leest();
+       
+       //BasisVariabelen basis = new BasisVariabelen();
+       //basis.Using();
+       
     }
 }
 
@@ -72,8 +77,8 @@ class Lezen
     public void Leest()
     {
         //8
-            string userName = Console.ReadLine();
-            Console.WriteLine("Hello {0}!", userName);
+        string userName = Console.ReadLine();
+        Console.WriteLine("Hello {0}!", userName);
         //9
         Console.WriteLine("\nHoe heet je?");
         String naam = Console.ReadLine();
@@ -83,10 +88,98 @@ class Lezen
         string UserName = Console.ReadLine();
         Console.WriteLine("Hello {0}!", userName);
         //11
-        
-        
-        
-        
+        string userNamer = "Gebruikersnaam";
+        Console.WriteLine(userNamer);
+        Console.ReadLine();
+        //12 
+        Console.Write("voer uw leeftijd");
+        string Waarde = Console.ReadLine();
+        if (int.TryParse(Waarde, out int UserInput))
+        {
+            Console.WriteLine("Leeftijd shown");
+        }
+        else
+        {
+            Console.WriteLine("Leeftijd niet geldig");
+        }
+
+        //13
+        int leeftijd = int.Parse(Console.ReadLine());
+        Console.WriteLine("oke u bent {0} jaar oud.", leeftijd);
+        //14
+        Console.Write("Voer uw leeftijd in: ");
+
+        if (int.TryParse(Console.ReadLine(), out int userAge))
+        {
+            Console.WriteLine("Leeftijd opgeslagen: " + userAge);
+        }
+        else
+        {
+            Console.WriteLine("Ongeldige invoer. Voer een geldig getal in voor leeftijd.");
+        }
+        //15
+        Random rand = new Random();
+
+        bool geraden = false;
+
+        int getalRaden = rand.Next(0, 10);
+        Console.WriteLine("Raad het getal van 0 tot 100 helemaal willekeurig");
+
+        while (!geraden)
+        {
+            try
+            {
+                int getalInvoer = int.Parse(Console.ReadLine());
+
+                if (getalInvoer == getalRaden)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Wow goed geraden! Het getal was inderdaad: " + getalRaden);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Helaas, niet het juiste getal. Probeer opnieuw!");
+                    Console.ResetColor();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Voer een geldig getal in.");
+            }
+        }
     }
-    
+}
+
+class BasisVariabelen
+{
+    public void Using()
+    {
+        //16 en 17
+        int integer = 10;
+        bool controle = true;
+        double doubleSpeed = 0.893234;
+        string spiderString = "John Doe";
+        float doesFloating = 250f;
+        //18
+        integer = 45;
+        doubleSpeed = 0.100;
+        //19
+        int integer2 = 50;
+        //20
+        Console.WriteLine("Variabeel integer2 = " + integer2);
+        // 21 
+        //je krijgt de variabeel te zien in de console als waarde, omdat je in de console.writeline de variabeel in de console ziet.
+    }
+}
+
+class BewerkingVariabelen
+{
+    public void bewerking()
+    {
+        //22
+    }
 }
